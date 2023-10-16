ac6ada8f0d0f2107303aac42f2a4f9cd2808f15a
gt1796926725 <1796926725@qq.com>
2023-10-16 19:34:44 +0800

    'mysql配置'

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -73,13 +73,22 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default':{
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
+        "USER": 'django_blog',
+        'PASSWORD': '123456', # 数据库用户密码
+        'NAME': 'django_blog' # 数据库名字
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
